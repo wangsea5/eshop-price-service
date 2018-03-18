f7899f0277161a7d4e7096d91b9f1e08e266303e
admin <1015517281@qq.com>
2018-03-18 16:26:35 +0800

    增加接口

--- src/main/java/com/roncoo/eshop/price/mapper/ProductPriceMapper.java
+++ src/main/java/com/roncoo/eshop/price/mapper/ProductPriceMapper.java
@@ -23,7 +23,7 @@ public interface ProductPriceMapper {
 	@Select("SELECT * FROM product_price WHERE id=#{id}")  
 	public ProductPrice findById(Long id);
 	
-	@Select("SELECT * FROM product_price product_id=#{productId}")
+	@Select("SELECT * FROM product_price WHERE product_id=#{productId}")
 	public ProductPrice findByProductId(Long productId);
 	
 }
